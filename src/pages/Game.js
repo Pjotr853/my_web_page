a4c0eea78de4cd09afeb68f93f440205c27b77ca
Peter Kizek <peter.kizek@student.tuke.sk>
2024-10-17 09:18:20 +0200

    begining of Game.js draw tile

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,46 @@
+import React, { useRef, useEffect, useState } from 'react';
+import './Game.css';
 
 function Game(){
 
+    const canvasRef = useRef(null);
+    const colorRectangel="grey";
+    const widthRectangel=30;
+    const heightRectangel=30;
+    const canvasWidht= 600;
+    const canvasHeight= 600;
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    function drawRectangel(ctx, x,y,width,height,color){
+        ctx.fillStyle=colorRectangel;
+        ctx.fillRect(x,y,width,height);  
+    }
+
+    function drawPlayGround(ctx){
+        drawRectangel()
+
+    }
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+
+        // Nastavenie rozmerov canvasu
+        canvas.width = canvasWidht;
+        canvas.height = canvasHeight;
+
+        // Kreslenie štvorca
+        drawRectangel(ctx, x,y,widthRectangel,heightRectangel,colorRectangel);
+        //ctx.fillStyle = 'blue';
+       // ctx.fillRect(50, 50, 100, 100);
+    }, []);
+
+
     return(
         <div>
-            Hello Game
+            <h1>Hello Game</h1>
+            <canvas ref={canvasRef} className='borderCanvas' />
         </div>
     );
 }
