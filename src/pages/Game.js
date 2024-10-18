5ee3a0672508bc76b2c12145e73a8b21ab74af7b
Peter Kizek <peter.kizek@student.tuke.sk>
2024-10-18 17:12:05 +0200

    pridanie chvosta, generovanie jedla a score

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import './Game.css';
 
-function Game(){
+function Game() {
 
     const canvasRef = useRef(null);
 
@@ -14,44 +14,54 @@ function Game(){
     const tileCountWidth = canvasWidht / widthRectangel;
     const tileCounthHeigh = canvasHeight / heightRectangel;
 
-    const [snakePositionX, setSnakePositionX] = useState(4*widthRectangel);
-    const [snakePositionY, setSnakePositionY] = useState(canvasHeight/2);
-    const [directX, setDirectX] = useState(1); 
+    const [snakePositionX, setSnakePositionX] = useState(4 * widthRectangel);
+    const [snakePositionY, setSnakePositionY] = useState(canvasHeight / 2);
+    const [directX, setDirectX] = useState(1);
     const [directY, setDirectY] = useState(0);
     const speed = 100;
+    const [snakeLenght, setSnakeLenght] = useState(3);
+    const [tail, setTail] = useState([
+        { x: 4 * widthRectangel - widthRectangel, y: canvasHeight / 2 }
+    ]);
 
     const [isPress, setIsPress] = useState(false);
-   // const [isIntervalRunning, setIsIntervalRunning] = useState(false);
 
-   const [foodX, setFoodX] =useState(canvasWidht-2*widthRectangel);
-   const [foodY, setFoodY] =useState(canvasHeight/2);
-    
+    const [foodX, setFoodX] = useState(canvasWidht - 2 * widthRectangel);
+    const [foodY, setFoodY] = useState(canvasHeight / 2);
 
-    function drawRectangel(ctx, x, y, width, height, color){
+    const [score, setScore] = useState(0);
+
+    function drawRectangel(ctx, x, y, width, height, color) {
         ctx.fillStyle = color;
-        ctx.fillRect(x, y, width, height);  
+        ctx.fillRect(x, y, width, height);
     }
 
-    function drawPlayGround(ctx){
+    function drawPlayGround(ctx) {
         drawRectangel(ctx, 0, 0, canvasWidht, canvasHeight, "silver");
 
-        for(let i = 0; i < tileCountWidth; i++){
-            for(let j = 0; j < tileCounthHeigh; j++){
+        for (let i = 0; i < tileCountWidth; i++) {
+            for (let j = 0; j < tileCounthHeigh; j++) {
                 drawRectangel(ctx, widthRectangel * i, heightRectangel * j, widthRectangel - 1, heightRectangel - 1, "white");
             }
         }
 
-        //food
-        drawRectangel(ctx,foodX,foodY,widthRectangel,heightRectangel,"green");
+        // food
+        drawRectangel(ctx, foodX, foodY, widthRectangel, heightRectangel, "green");
     }
 
-    function animation(ctx){
+    function animation(ctx) {
         drawPlayGround(ctx);
         drawRectangel(ctx, snakePositionX, snakePositionY, widthRectangel, heightRectangel, "red");
+
+        tail.forEach(segment => {
+            drawRectangel(ctx, segment.x, segment.y, widthRectangel, heightRectangel, "blue");
+        });
+
+        
     }
 
     function handleKeyDown(e) {
-        if(!isPress){
+        if (!isPress) {
             setIsPress(true);
             if ((e.key === 'w' || e.key === 'ArrowUp') && directY !== 1) {
                 setDirectX(0);
@@ -67,7 +77,6 @@ function Game(){
                 setDirectY(0);
             }
         }
-        
     }
 
     function moveSnake() {
@@ -82,10 +91,6 @@ function Game(){
             return newX;
         });
 
-        function resetFood(){
-
-        }
-    
         setSnakePositionY(prevY => {
             const newY = prevY + directY * heightRectangel;
             if (newY < 0) {
@@ -96,7 +101,40 @@ function Game(){
             }
             return newY;
         });
+        snakeTail();
     }
+
+    function snakeTail() {
+        
+        setTail(prevTail => {
+            const newTail = [...prevTail, { x: snakePositionX, y: snakePositionY }];
+            return newTail.slice(-snakeLenght);
+        });
+    }
+
+    function eatFood(){
+
+        if(snakePositionX==foodX && snakePositionY==foodY){
+            setSnakeLenght(snakeLenght+1);
+            setScore(score+1);
+            generateFood();
+        }
+    }
+    
+    function generateFood(){
+        const newFoodX = Math.floor(Math.random() * 16) * widthRectangel;
+        const newFoodY = Math.floor(Math.random() * 16) * heightRectangel;
+        
+        const isCollidingWithSnake = tail.some(segment => segment.x === newFoodX && segment.y === newFoodY);
+        
+        if ((newFoodX === snakePositionX && newFoodY === snakePositionY)||isCollidingWithSnake) {
+            generateFood(); 
+        } else {
+            setFoodX(newFoodX);
+            setFoodY(newFoodY);
+        }
+
+    };
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -105,28 +143,25 @@ function Game(){
         canvas.width = canvasWidht;
         canvas.height = canvasHeight;
 
-       // setIsIntervalRunning(true);
         const interval = setInterval(() => {
             moveSnake();
-        }, speed); 
+            eatFood();
+           // snakeTail();
+        }, speed);
 
-
-        window.addEventListener('keydown', handleKeyDown); 
+        window.addEventListener('keydown', handleKeyDown);
         animation(ctx);
 
         return () => {
-            clearInterval(interval); 
+            clearInterval(interval);
             window.removeEventListener('keydown', handleKeyDown);
             setIsPress(false);
-          //  setIsIntervalRunning(false); 
         };
-    }, [directX, directY,snakePositionX, snakePositionY]); 
+    }, [directX, directY, snakePositionX, snakePositionY]);
 
-    
-
-    return(
+    return (
         <div>
-            <h1>Hello Game</h1>
+            <h1>Hello Game</h1> <h2>Score {score}</h2>
             <canvas ref={canvasRef} className='borderCanvas' />
         </div>
     );
